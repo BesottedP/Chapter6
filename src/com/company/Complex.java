4ce3cab3df54fa12a3a850f80e2b5538d4c552ba
besottedpotato <ishaanpatel@billericak12.com>
2022-01-05 21:28:10 -0500

    January 5, 9:27 PM

--- src/com/company/Complex.java
+++ src/com/company/Complex.java
@@ -26,13 +26,25 @@ public class Complex {
         return nComplex;
     }
 
+    public Complex add(double other){
+        double r = real + other;
+        Complex nComplex = new Complex(r);
+        return nComplex;
+    }
 
-    public static void main(String[] args) {
-        Complex x = new Complex(7.0, 2.0);
-        Complex y = new Complex(5.0, 4.0);
-        System.out.println(x.abs());
-        System.out.println(y.abs());
-        System.out.println(x.add(y));
+
+    public Complex multiply(Complex other){
+        double r = (real * other.real) - (imagine * other.imagine);
+        double i = (real * other.imagine) + (imagine * other.real);
+        Complex qComplex = new Complex(r,i);
+        return qComplex;
+    }
+
+    public Complex multiply(double other){
+        double r = (real * other);
+        double i = (imagine * other);
+        Complex qComplex = new Complex(r,i);
+        return qComplex;
     }
 
     public String toString()
@@ -42,4 +54,28 @@ public class Complex {
     }
 
 
+    public static void main(String[] args) {
+        Complex x = new Complex(7.0, 2.0);
+        Complex y = new Complex(5.0, 4.0);
+        Complex z = new Complex(0.0, 1.0);
+        Complex xy = new Complex(2.0, -4.5);
+        Complex xz = new Complex(6.0);
+        Complex yz = new Complex(3.0);
+
+        System.out.println("abs");
+        System.out.println(x.abs());
+        System.out.println(z.abs());
+        System.out.println(xy.abs());
+        System.out.println();
+        System.out.println("add");
+        System.out.println(x.add(y));
+        System.out.println(z.add(5.0));
+        System.out.println();
+        System.out.println("multiply");
+        System.out.println(x.multiply(y));
+        System.out.println(xz.multiply(x));
+        System.out.println(yz.multiply(4.0));
+    }
 }
+
+//Should the method names be the same of different?
